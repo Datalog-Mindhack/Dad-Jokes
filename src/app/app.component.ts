b4e2cd55fdad8fec7f31fe581a8da7457047ae58
Datalog-Mindhack <vandanajha92283@gmail.com>
2022-02-23 11:04:42 +0530

    changes in app.component.ts and app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'icanhazdad';
+  
+  ngOnInit(): void {
+    let getJokeBtn = document.getElementById('get_joke');
+    let joke = document.getElementById('joke');
+    let new_joke : string;
+
+    getJokeBtn?.addEventListener('click', async () => {
+        const dadJoke = await fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                Accept: "application/json"
+            }
+        });
+        const dadJokeJSON = await dadJoke.json();
+        if (dadJokeJSON.status === 200) {
+          (async () => {
+            new_joke = dadJokeJSON.joke;
+            // console.log(new_joke);
+            joke!.innerText = new_joke;
+          })();
+        } else {
+           console.log("Error retrieving dad joke!");
+        }
+    })
+  }
 }
